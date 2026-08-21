eb5c23cc8d9574836ac76e94093faa990b8e8a3e
Sana-Msallam <sana.2522005@gmail.com>
2026-08-21 17:41:17 +0300

    no

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -2,7 +2,7 @@ class ApiConstants {
   ApiConstants._();
 
   static const String baseUrl =
-      'https://mill-respectively-musicians-preferences.trycloudflare.com';
+      'https://optimum-fixed-earthquake-thomas.trycloudflare.com';
   // static const String baseUrl = 'http://192.168.1.13:3000';
 
   static const String login = '/auth/login/client';
